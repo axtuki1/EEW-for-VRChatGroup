b080f3803c163ad42087b43782c4d83ba4ae3d4d
axtuki1 <master@axtuki1.com>
2024-07-06 23:50:38 +0900

    update 受信後の送信処理を追加。
    一旦テスト稼働できそう

--- src/CheckEarthquake_DMDATA.ts
+++ src/CheckEarthquake_DMDATA.ts
@@ -5,6 +5,7 @@ import { Logger } from "./util/logger";
 import rndstr from "rndstr";
 import { DMDATA } from "./dmdata/DMDATA";
 const WebSocket = require("ws");
+const zlib = require("zlib");
 const expressWs = require('express-ws');
 const { parse } = require("jsonc-parser");
 const config = (() => {
@@ -22,93 +23,59 @@ export class CheckEarthquake_DMDATA extends CheckEarthquake {
     private dmdata: DMDATA;
     private isReconnect = true;
     public intensityTable = {
-        "-1": 1,
+        "不明": 1,
         "0": 2,
-        "10": 3,
-        "20": 4,
-        "30": 5,
-        "40": 6,
-        "50": 7,
-        "55": 8,
-        "60": 9,
-        "65": 10,
-        "70": 11,
+        "1": 3,
+        "2": 4,
+        "3": 5,
+        "4": 6,
+        "5+": 7,
+        "5-": 8,
+        "6-": 9,
+        "6+": 10,
+        "7": 11,
+        "over": 12,
     };
     public noticeIntensity = 6;
     public intensityNameMaster = {
-        "-1": "不明",
+        "不明": "不明",
         "0": "0",
-        "10": "1",
-        "20": "2",
-        "30": "3",
-        "40": "4",
-        "50": "5弱",
-        "55": "5強",
-        "60": "6弱",
-        "65": "6強",
-        "70": "7",
+        "1": "1",
+        "2": "2",
+        "3": "3",
+        "4": "4",
+        "5-": "5弱",
+        "5+": "5強",
+        "6-": "6弱",
+        "6+": "6強",
+        "7": "7",
+        "over": "7",
     }
     private retryCount = 0;
 
     private func = {
-        551: (inputData) => { // JMAQuake 地震情報
-            console.log("test");
-        },
-        554: (inputData) => { // 緊急地震速報検出
-            this.callback(config.EEWDetectData.Title, config.EEWDetectData.Body, config.EEWDetectData.Popup);
-        },
-        556: (inputData) => { // EEW
-            let data = {
-                is_training: inputData.test,
-                alertflg: true,
-                is_cancel: inputData.cancelled,
-                region_name: "-",
-                intensity: "-",
-                magunitude: "-",
-                depth: "-",
-                origin_time: inputData.issue.time
-            };
-            if (!inputData.cancelled) {
-                data.region_name = inputData.earthquake.name;
-                data.intensity = inputData.areas.reduce((a, b) => Math.max(a.scaleTo, b.scaleTo));
-                data.magunitude = inputData.earthquake.magnitude;
-                data.depth = inputData.earthquake.depth + "km";
-                data.origin_time = inputData.earthquake.originTime
+        "VXSE45": (data, xmlData) => {
+            if (xmlData.body.earthquake.condition == "仮定震源要素時") {
+                // 仮定震源要素時は無視
+                return;
             }
-            this.SendData(data);
-            /**
-             * {
-             *   id: "情報を一意に識別するID",
-             *   code: 556,
-             *   time: "2006/01/02 15:04:05.999", // 受信日時
-             *   test: true // テストかどうか,
-             *   earthquake: {
-             *     originTime: "地震発生時刻",
-             *     arrivalTime: "地震発現時刻",
-             *     condition: "", // 仮定震源要素の場合、値は "仮定震源要素"
-             *     hypocenter: {
-             *       name: "震央地名",
-             *       reduceName: "短縮用震央地名",
-             *       latitude: 0, // 緯度
-             *       longitude: 0, // 経度
-             *       depth: 10.0, // 深さ(km) 存在しない場合は-1 整数部のみ有効
-             *       magnitude: 5.3 // マグニチュード 存在しない場合は-1
-             *     }
-             *   },
-             *   issue: {
-             *     time: "発表時刻",
-             *     eventId: "識別情報",
-             *     serial: "情報番号"
-             *   },
-             *   cancelled: false, // キャンセル報か
-             *   area: [ // 細分区域 最大震度の情報あり
-             *     {
-             *       scaleFrom: 50, // 最大震度の下限
-             *       scaleTo: 50 // 最大震度の上限
-             *     }
-             *   ]
-             * }
-             */
+            // 通知対象の震度か確認
+            if (this.intensityTable[xmlData.intensity.forecastMaxInt.to] < this.noticeIntensity) {
+                return;
+            }
+            const outputData = {
+                is_training: xmlData.body.earthquake,
+                is_final: xmlData.body.earthquake.isLastInfo,
+                is_cancel: xmlData.body.earthquake.isCanceled,
+                alertflg: xmlData.body.isWarning ? "警報" : "予報", // 緊急地震速報（警報）発報時に"警報"
+                report_num: xmlData.serialNo,
+                region_name: xmlData.body.earthquake.hypocenter.name,
+                calcintensity: this.intensityNameMaster[xmlData.body.intensity.forecastMaxInt.to],
+                magunitude: xmlData.body.earthquake.magunitude.value ? xmlData.body.earthquake.magunitude.value : "不明",
+                depth: xmlData.earthquake.hypocenter.depth,
+                origin_time: xmlData.body.earthquake.originTime
+            };
+            this.SendData(outputData);
         }
     }
 
@@ -117,18 +84,8 @@ export class CheckEarthquake_DMDATA extends CheckEarthquake {
         this.logger = new Logger("DMDATA");
     }
 
-    private idDeadlineTimeSec = 30;
-    private registPostId(id) {
-        this.recvIds.push(id);
-        setTimeout(() => {
-            this.recvIds = this.recvIds.filter(currentId => currentId !== id);
-        }, this.idDeadlineTimeSec * 1000);
-    }
-    private checkPostId(id) {
-        return this.recvIds.indexOf(id) !== -1;
-    }
     public async connect() {
-        
+
         this.logger.log("Creating ticket...");
         const ticket = await this.dmdata.createTicket(
             "EEWForVRChatGroup",
@@ -139,10 +96,10 @@ export class CheckEarthquake_DMDATA extends CheckEarthquake {
             [
                 "VXSE44", // 緊急地震速報（予報）
                 "VXSE45", // 緊急地震速報（地震動予報）
-                "VXSE51", // 震度速報
-                "VTSE41", // 津波警報・注意報・予報
-                "VXSE52", // 震源に関する情報
-                "VXSE53"  // 震源・震度に関する情報
+                // "VXSE51", // 震度速報
+                // "VTSE41", // 津波警報・注意報・予報
+                // "VXSE52", // 震源に関する情報
+                // "VXSE53"  // 震源・震度に関する情報
             ],
             "json",
             true
@@ -177,7 +134,7 @@ export class CheckEarthquake_DMDATA extends CheckEarthquake {
                 this.logger.log("Reconnecting...");
                 this.connect();
             }
-        
+
         });
         this.dmdata.addEventListener("close", () => {
             this.logger.log("Disconnected from DMDATA!");
@@ -188,11 +145,15 @@ export class CheckEarthquake_DMDATA extends CheckEarthquake {
         });
         this.dmdata.addEventListener("receive", (data) => {
             this.lastResponse = data;
+            if (data.body != null) {
+                // gzip圧縮されているデータを解凍
+                data.body = JSON.parse(zlib.gunzipSync(Buffer.from(data.body, 'base64')).toString('utf-8'));
+            }
             this.DataProcess(data);
         });
-        
+
         this.connect();
-        
+
     }
     public Stop() {
         // 停止時処理...
@@ -201,14 +162,15 @@ export class CheckEarthquake_DMDATA extends CheckEarthquake {
     }
     // データ処理 試験データもここに来るので...
     public DataProcess(data) {
-        const func = this.func[data.code];
-        if (func != null) func(data);
+
+        const func = this.func[data.head.type];
+        if (func != null) func(data, data.body);
         if (config.gatherData) {
             let nowTime = new Date().toLocaleDateString("ja-JP", {
                 year: "numeric", month: "2-digit",
                 day: "2-digit", hour: "2-digit", minute: "2-digit", second: "2-digit"
             });
-            
+
             console.log("Data received: " + nowTime);
 
             if (!fs.existsSync("secret/gather")) {
@@ -218,9 +180,30 @@ export class CheckEarthquake_DMDATA extends CheckEarthquake {
             fs.writeFileSync(`secret/gather/${time}-${rndstr("0123456789", 4)}.json`, JSON.stringify(data, null, 3));
         }
     }
+
+    /*
+    {
+        is_training: false,
+        alertflg: "予報", // 緊急地震速報（警報）発報時に"警報"
+        report_num: 1,
+        region_name: "東京都南部",
+        calcintensity: "30",
+        magunitude: "3.9",
+        depth: "40",
+        origin_time: "20210616102424"
+    }
+    */
+    private lastData = {};
     public SendData(data) {
         const origin_time = data.origin_time.replaceAll(/([0-9]{4})([0-9]{2})([0-9]{2})([0-9]{2})([0-9]{2})([0-9]{2})/g, "$1年$2月$3日 $4:$5:$6");
-        let sendMsg = config.settings.sendMsg;
+        let sendMsg = config.settings.DMDATA.sendMsg;
+        if (data.is_cancel) {
+            sendMsg = config.settings.DMDATA.cancelMsg;
+            data = this.lastData;
+        } else {
+            this.lastData = data;
+        }
+        
         sendMsg = sendMsg.replaceAll("${isTraining}", data.is_training ? "--訓練-- " : "");
         sendMsg = sendMsg.replaceAll("${alertFlg}", data.alertflg == "警報" ? "!警報! " : "");
         sendMsg = sendMsg.replaceAll(
@@ -256,6 +239,14 @@ export class CheckEarthquake_DMDATA extends CheckEarthquake {
             this.DataProcess(data);
             res.json({
                 status: "ok"
+            });
+        });
+        router.post("/api/v1/gunziptest", (req, res) => {
+            const data = req.body;
+            data.body = JSON.parse(zlib.gunzipSync(Buffer.from(data.body, 'base64')).toString('utf-8'));
+            res.json({
+                status: "ok",
+                result: data
             });
         });
         router.get("/api/v1/testAnnounce", async (req, res) => {
