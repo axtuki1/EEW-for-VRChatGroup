8cc53947ca0c3941db470cc4cb8dcdfe5d7200ec
axtuki1 <master@axtuki1.com>
2026-02-07 05:08:20 +0900

    update 津波機能が無効の状態は「地震・津波関連 区分」の要求をしないように変更

--- src/CheckEarthquake_DMDATA.ts
+++ src/CheckEarthquake_DMDATA.ts
@@ -149,21 +149,23 @@ export class CheckEarthquake_DMDATA extends CheckEarthquake {
             this.dmdata.closeConnect(this.currentTicket);
         }
 
+        const config = Config.get();
+
         this.logger.info("Creating ticket...");
         const ticket = await this.dmdata.createTicket(
             "EEWForVRChatGroup",
             [
                 DMDATA.Ticket.Classification.EEW.Forecast,
-                DMDATA.Ticket.Classification.Telegram.Earthquake
-            ],
+                config.features.enableTsunamiAlert_VTSE41 ? DMDATA.Ticket.Classification.Telegram.Earthquake : null
+            ].filter(item => item !== null),
             [
                 "VXSE44", // 緊急地震速報（予報）
                 "VXSE45", // 緊急地震速報（地震動予報）
                 // "VXSE51", // 震度速報
-                "VTSE41", // 津波警報・注意報・予報
+                config.features.enableTsunamiAlert_VTSE41 ? "VTSE41" : null, // 津波警報・注意報・予報
                 // "VXSE52", // 震源に関する情報
                 // "VXSE53"  // 震源・震度に関する情報
-            ],
+            ].filter(item => item !== null),
             "json",
             false // テスト報の受けとり
         );
